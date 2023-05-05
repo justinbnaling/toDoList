0913ccf179e89a959f62d93e4630cb729752b6e7
Justin Naling <justinbnaling@gmail.com>
2023-05-05 18:01:51 -0500

    first commit with npm and webpack environment created

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function component() {
+    const element = document.createElement('div');
+    
+    element.textContent = "Hello World"  
+    return element;
+  }
+  
+  document.body.appendChild(component());
